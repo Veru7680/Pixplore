0421aed7256c5999bfd20de1f71f541a4f8fc8e6
Veru7680 <veru6198@gmail.com>
2024-09-19 14:55:56 -0600

    se agrego diseño y inicio aleatorio de img

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
+// Elementos del DOM
 const BuscarB = document.getElementById('BuscarB');
 const BuscarI = document.getElementById('BuscarI');
 const imageResults = document.getElementById('imageResults');
+const categoryButtons = document.querySelectorAll('.category-btn');
 
 // Función para buscar imágenes en Unsplash-API
 async function BuscarImages(query) {
@@ -32,10 +33,42 @@ function displayImages(images) {
     }
 }
 
-// evento de búsqueda al botón
+// Evento de búsqueda al botón
 BuscarB.addEventListener('click', () => {
     const query = BuscarI.value.trim();
     if (query !== '') {
         BuscarImages(query);
     }
 });
+
+// Eventos para los botones de categorías
+categoryButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const category = button.getAttribute('data-category');
+        BuscarImages(category);
+    });
+});
+
+// Generador de palabras aleatorias
+function generarPalabraAleatoria() {
+    const palabras = ['paisaje', 'animales', 'comida', 'dibujos', 'naturaleza', 'tecnología', 'deportes', 'ciudades', 'viajes', 'montañas',
+'bosques', 'ríos', 'cielos', 'mares', 'cultura', 'historia', 'arquitectura', 'fotografía', 'arte', 'desiertos',
+'playas', 'jardines', 'parques', 'bosques', 'selvas', 'campos', 'insectos', 'mamíferos', 'pájaros', 'peces',
+'reptiles', 'anfibios', 'perros', 'gatos', 'caballos', 'vehículos', 'trenes', 'aviones', 'barcos', 'bicicletas',
+'robots', 'inteligencia artificial', 'computadoras', 'redes', 'internet', 'aplicaciones', 'programación', 'videojuegos',
+'automóviles', 'fútbol', 'baloncesto', 'natación', 'esquí', 'snowboard', 'boxeo', 'ciclismo', 'senderismo', 'tenis',
+'golf', 'vela', 'patinaje', 'atletismo', 'escalada', 'maratón', 'ciudad', 'edificios', 'rascacielos', 'carreteras',
+'tráfico', 'metro', 'aeropuertos', 'puentes', 'plazas', 'monumentos', 'fuentes', 'plazas', 'mercados', 'ferias',
+'teatros', 'conciertos', 'exposiciones', 'pintura', 'escultura', 'cine', 'documentales', 'series', 'paisajes urbanos',
+'pueblos', 'aldeas', 'castillos', 'iglesias', 'templos', 'museos', 'acantilados', 'volcanes', 'cascadas', 'lago',
+'tormentas', 'clima', 'fauna', 'flora', 'pueblos indígenas'];
+    const indiceAleatorio = Math.floor(Math.random() * palabras.length);
+    return palabras[indiceAleatorio];
+}
+
+// Mostrar imágenes al cargar la página con una palabra aleatoria
+window.addEventListener('DOMContentLoaded', () => {
+    const palabraAleatoria = generarPalabraAleatoria();
+    BuscarImages(palabraAleatoria); // Buscar imágenes con una palabra aleatoria
+});
+ 
